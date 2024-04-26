ac311e3d02abb2f0606179f5bfe7708b609c076b
Mark <markchan0134@gmail.com>
2024-04-27 03:08:21 +0800

    Header + Sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Observable, map, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'container-tracking-system';
+  
+  @ViewChild('sidenav') sidenav?: MatSidenav;
+  isSmallScreen? : boolean;
+  
+  buttonItems: { 
+    icon: string, 
+    text: string, 
+    link: string 
+  }[] = [
+    { icon: 'add', text: 'New Order', link: '/' },
+    { icon: 'account_circle', text: 'Update Maindata', link: '/profile' },
+    { icon: 'settings', text: 'Payment List', link: '/settings' },
+    { icon: 'help', text: 'Search Records', link: '/help' },
+    { icon: 'home', text: 'Print Automated C.N.', link: '/' },
+    { icon: 'account_circle', text: 'Client Billing', link: '/profile' },
+    { icon: 'settings', text: 'Offhire Listing', link: '/settings' },
+  ];
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+  toggleSidenav() {
+    if (this.sidenav) {
+      this.sidenav.toggle();
+    }
+  }
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+
+    this.breakpointObserver.observe([
+      Breakpoints.Small,
+      Breakpoints.XSmall
+    ]).subscribe(result => {
+      this.isSmallScreen = !result.matches;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -10,10 +18,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
