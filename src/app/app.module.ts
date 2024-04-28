3be0bad9177fbf6498ce247e7e641a3684ff4b9f
Mark <markchan0134@gmail.com>
2024-04-28 19:35:06 +0800

    Create create-order component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,32 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-// Angular Material
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+// Components
+import { CreateOrderComponent } from './create-order/create-order.component';
+
+// routes
+const routes: Routes = [
+  { path: '', redirectTo: '/create-order', pathMatch: 'full' }, // Default route
+  { path: 'create-order', component: CreateOrderComponent }, // Route for CreateOrderComponent
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateOrderComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +39,11 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [
     provideClientHydration(),
